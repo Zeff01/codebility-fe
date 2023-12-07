4a4693d3b3e9ceba2530232d03c7328edb2d5c6b
David <david.estrelloso.tribugenia@gmail.com>
2023-12-07 12:00:27 +0800

    Remove Imports

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,5 @@
 import "./globals.css"
 import { ThemeProvider } from "@/components/ui/theme-provider"
-import { ThemeModeToggle } from "@/components/ui/thememodetoggle"
-import ToggleMenu from "Components/toggleMenu"
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
